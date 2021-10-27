326dc9763ac7c3c5a37195127b92ec3aa0a5f64a
dannyboy9934 <50834782+dannyboy9934@users.noreply.github.com>
2021-10-27 10:11:29 +0200

    Update numberToPosition.ts

--- numberToPosition.ts
+++ numberToPosition.ts
@@ -1,16 +1,16 @@
 export interface position {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 }
-
+  
 export const numToPos: Array<position> = [
-  { x: 0, y: 0 },
-  { x: 0, y: 1 },
-  { x: 0, y: 2 },
-  { x: 1, y: 0 },
-  { x: 1, y: 1 },
-  { x: 1, y: 2 },
-  { x: 2, y: 0 },
-  { x: 2, y: 1 },
-  { x: 2, y: 2 },
+	{ x: 0, y: 0 },
+	{ x: 0, y: 1 },
+	{ x: 0, y: 2 },
+	{ x: 1, y: 0 },
+	{ x: 1, y: 1 },
+	{ x: 1, y: 2 },
+	{ x: 2, y: 0 },
+	{ x: 2, y: 1 },
+	{ x: 2, y: 2 },
 ];
